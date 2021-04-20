a7bb3bf9efa24ee32740f0cf6f885ee0410c5173
Farid Shabonov <farid.shabonov@flexaspect.com>
2021-04-20 20:43:03 +0300

    Initial Commit

---
+++ Shared/ipInformerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ipInformerApp.swift
+//  Shared
+//
+//  Created by Farid Shabonov on 20.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct ipInformerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
